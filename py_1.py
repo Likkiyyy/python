28cc02c13f07542174c32037dec1022b994320a1
Likkiyyy <likkiyyy.1@gmail.com>
2023-02-20 17:51:32 +0900

    @01

--- py_1.py
+++ py_1.py
@@ -4,12 +4,13 @@ box3 = 6
 i_num = None
 i_box = None
 
-
 while True:
-    print(box1,box2,box3)
+    print(box1, box2, box3)
     try:
-        i_box,i_num =(int(x) for x in input().split())
+        i_box, i_num = (int(x) for x in input(">>>").split())
     except IndexError:
+        print("Error")
+    except ValueError:
         print("Error")
     else:
         if i_box == 1 and i_num <= box1:
@@ -18,3 +19,5 @@ while True:
             box2 -= i_num
         if i_box == 3 and i_num <= box3:
             box3 -= i_num
+        if box1 == 0 and box2 == 0 and box3 == 0:
+            break
